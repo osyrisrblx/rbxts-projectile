1bdc0eb052594f3037b8aa09c02dc8325e1ba2f6
osyrisrblx <osyrisrblx@gmail.com>
2019-12-08 22:04:02 -0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# rbxts-projectile
+# @rbxts/projectile
+
+WIP projectile package
+
+Creates CylinderHandleAdornment objects in Workspace.Terrain
+
+TODO:
+	- allow for models for stuff like grenades, etc.
+
+Simple demo:
+```TS
+// main.client.ts
+
+import { Players } from "@rbxts/services";
+import { Projectile } from "shared/Projectile";
+
+const SHOT_COUNT = 3;
+const MIN_SPREAD_ANGLE = math.rad(0);
+const MAX_SPREAD_ANGLE = math.rad(10);
+
+const random = new Random();
+
+const mouse = Players.LocalPlayer.GetMouse();
+
+let tag: object | undefined;
+
+mouse.Button1Down.Connect(() => {
+	const myTag = {};
+	tag = myTag;
+
+	while (tag === myTag) {
+		const character = Players.LocalPlayer.Character;
+		if (!character) break;
+
+		const head = character.FindFirstChild("Head");
+		if (!head || !head.IsA("BasePart")) break;
+
+		const position = head.CFrame.Position;
+		const endPos = mouse.Hit.Position;
+
+		for (let i = 0; i < SHOT_COUNT; i++) {
+			const velocity = new CFrame(position, endPos)
+				.mul(CFrame.Angles(0, 0, random.NextNumber(0, 2 * math.pi))) // roll to a random angle
+				.mul(CFrame.Angles(0, random.NextNumber(MIN_SPREAD_ANGLE, MAX_SPREAD_ANGLE), 0))
+				.LookVector.mul(75);
+
+			new Projectile({
+				position,
+				velocity,
+				color: Color3.fromHSV(math.random(), 1, 1),
+				life: 4,
+				bounce: true,
+				acceleration: new Vector3(0, -50, 0),
+				physicsIgnore: [character],
+				penetration: true,
+				minExitVelocity: 50
+			});
+		}
+		wait(1 / 30);
+	}
+});
+
+mouse.Button1Up.Connect(() => (tag = undefined));
+```
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,46 @@
 import { Workspace, RunService } from "@rbxts/services";
 
-const terrain = Workspace.Terrain;
-
-const raycastIgnore = Workspace.FindFirstChild("Ignore");
-
-type ProjectileConfig = Partial<{
+interface ProjectileConfig {
 	position: Vector3;
 	velocity: Vector3;
 	acceleration: Vector3;
 
-	bounce: boolean;
-	canCollide: boolean;
-	color: Color3;
-	life: number;
-	maxRange: number;
-	minExitVelocity: number;
-	penetration: boolean;
-	physicsIgnore: Array<Instance>;
-	resistance: number;
+	bounce?: boolean;
+	canCollide?: boolean;
+	color?: Color3;
+	life?: number;
+	maxRange?: number;
+	minExitVelocity?: number;
+	penetration?: boolean;
+	physicsIgnore?: Array<Instance>;
+	resistance?: number;
 
-	onTouch: (part: BasePart, position: Vector3, normal: Vector3) => void;
-}>;
-
-let elapsedTime = 0;
-const globalPhysicsIgnore = new Array<Instance>();
-if (raycastIgnore) {
-	globalPhysicsIgnore.push(raycastIgnore);
-}
-const removeList = new Set<Projectile>();
-const lines = new Array<CylinderHandleAdornment>();
-const particles = new Array<Projectile>();
-
-function getLine(color: Color3) {
-	const line = lines.pop() || new Instance("CylinderHandleAdornment");
-	line.Parent = terrain;
-	line.Adornee = terrain;
-	line.Color3 = color;
-	line.Radius = 0.05;
-	return line;
+	onTouch?: (part: BasePart, position: Vector3, normal: Vector3) => unknown;
 }
 
 export class Projectile {
-	public lifeTime: number;
-	public line: CylinderHandleAdornment;
+	public static elapsedTime = 0;
+	public static projectiles = new Array<Projectile>();
+	public static readonly removeList = new Set<Projectile>();
+	public static readonly globalPhysicsIgnore = new Array<Instance>();
+	public static readonly lines = new Array<CylinderHandleAdornment>();
 
-	private px = 0;
-	private py = 0;
-	private pz = 0;
+	public readonly lifeTime: number;
+	public readonly line: CylinderHandleAdornment;
 
-	private vx = 0;
-	private vy = 0;
-	private vz = 0;
+	private px: number;
+	private py: number;
+	private pz: number;
 
-	private ax = 0;
-	private ay = 0;
-	private az = 0;
+	private vx: number;
+	private vy: number;
+	private vz: number;
+
+	private ax: number;
+	private ay: number;
+	private az: number;
 
 	private distanceSq = 0;
-	private wasRendered = true;
 
 	private bounce: boolean;
 	private color: Color3;
@@ -68,230 +51,210 @@ export class Projectile {
 	private physicsIgnore: Array<Instance>;
 	private resistance: number;
 
-	private onTouch?: (part: BasePart, position: Vector3, normal: Vector3) => void;
+	private onTouch: ProjectileConfig["onTouch"];
 
-	public constructor({
-		position,
-		velocity,
-		acceleration,
-		bounce = false,
-		canCollide = false,
-		color = new Color3(1, 1, 1),
-		life = 2,
-		maxRange = 5000,
-		minExitVelocity = 100,
-		physicsIgnore = [],
-		penetration = false,
-		resistance = 1,
-		onTouch,
-	}: ProjectileConfig) {
-		if (position) {
-			this.px = position.X;
-			this.py = position.Y;
-			this.pz = position.Z;
-		}
+	public constructor(config: ProjectileConfig) {
+		// default values
+		const {
+			bounce = false,
+			canCollide = true,
+			color = new Color3(1, 1, 1),
+			life = 2,
+			maxRange = 5000,
+			minExitVelocity = 100,
+			physicsIgnore = [],
+			penetration = false,
+			resistance = 1,
+		} = config;
 
-		if (velocity) {
-			this.vx = velocity.X;
-			this.vy = velocity.Y;
-			this.vz = velocity.Z;
-		}
+		this.px = config.position.X;
+		this.py = config.position.Y;
+		this.pz = config.position.Z;
 
-		if (acceleration) {
-			this.ax = acceleration.X;
-			this.ay = acceleration.Y;
-			this.az = acceleration.Z;
-		}
+		this.vx = config.velocity.X;
+		this.vy = config.velocity.Y;
+		this.vz = config.velocity.Z;
+
+		this.ax = config.acceleration.X;
+		this.ay = config.acceleration.Y;
+		this.az = config.acceleration.Z;
 
 		this.bounce = bounce;
 		this.canCollide = canCollide;
 		this.color = color;
-		this.lifeTime = elapsedTime + life;
-		this.maxRangeSq = maxRange * maxRange;
+		this.lifeTime = Projectile.elapsedTime + life;
+		this.maxRangeSq = maxRange ** 2;
 		this.minExitVelocity = minExitVelocity;
-		this.physicsIgnore = [...globalPhysicsIgnore, ...physicsIgnore];
+		this.physicsIgnore = [...Projectile.globalPhysicsIgnore, ...physicsIgnore];
 		this.penetration = penetration;
 		this.resistance = resistance;
 
-		this.onTouch = onTouch;
+		this.onTouch = config.onTouch;
 
-		this.line = getLine(this.color);
+		this.line = Projectile.getLine(this.color);
 
-		particles.push(this);
+		Projectile.projectiles.push(this);
 	}
 
 	public static addToPhysicsIgnore(object: Instance) {
-		for (const v of globalPhysicsIgnore) {
+		for (const v of Projectile.globalPhysicsIgnore) {
 			if (v === object) {
 				return;
 			}
 		}
 
-		globalPhysicsIgnore.push(object);
+		Projectile.globalPhysicsIgnore.push(object);
 
 		object.GetPropertyChangedSignal("Parent").Connect(() => {
 			if (!object.Parent) {
-				const index = globalPhysicsIgnore.indexOf(object);
+				const index = Projectile.globalPhysicsIgnore.indexOf(object);
 				if (index !== -1) {
-					globalPhysicsIgnore.remove(index);
+					Projectile.globalPhysicsIgnore.remove(index);
 				}
 			}
 		});
 	}
 
+	private static getLine(color: Color3) {
+		const line = this.lines.pop() || new Instance("CylinderHandleAdornment");
+		line.Parent = Workspace.Terrain;
+		line.Adornee = Workspace.Terrain;
+		line.Color3 = color;
+		line.Radius = 0.05;
+		return line;
+	}
+
+	private static raycast(start: Vector3, direction: Vector3, ignore: Array<Instance>) {
+		return Workspace.FindPartOnRayWithIgnoreList(new Ray(start, direction), ignore, false, true);
+	}
+
 	public step(dt: number) {
-		const physicsIgnore = this.physicsIgnore;
+		// store original position
+		const pxOrig = this.px;
+		const pyOrig = this.py;
+		const pzOrig = this.pz;
 
-		const qx = this.px;
-		const qy = this.py;
-		const qz = this.pz;
+		// store original velocity
+		const vxOrig = this.vx;
+		const vyOrig = this.vy;
+		const vzOrig = this.vz;
 
-		const ix = this.vx;
-		const iy = this.vy;
-		const iz = this.vz;
-
+		// update velocity
 		this.vx += dt * this.ax;
 		this.vy += dt * this.ay;
 		this.vz += dt * this.az;
 
-		let dx = (dt / 2) * (ix + this.vx);
-		let dy = (dt / 2) * (iy + this.vy);
-		let dz = (dt / 2) * (iz + this.vz);
-
-		let dir = new Vector3(dx, dy, dz);
+		// expected delta
+		let dx = (dt * (vxOrig + this.vx)) / 2;
+		let dy = (dt * (vyOrig + this.vy)) / 2;
+		let dz = (dt * (vzOrig + this.vz)) / 2;
 
 		if (this.canCollide && (dx !== 0 || dy !== 0 || dz !== 0)) {
-			const [part, pos, norm] = Workspace.FindPartOnRayWithIgnoreList(
-				new Ray(new Vector3(this.px, this.py, this.pz), dir),
-				physicsIgnore,
-				false,
-				true,
-			);
+			const dir = new Vector3(dx, dy, dz);
+			const startPos = new Vector3(this.px, this.py, this.pz);
+			const [part, pos, norm] = Projectile.raycast(startPos, dir, this.physicsIgnore);
 			if (part) {
+				let didPenetrate = false;
 				if (this.penetration) {
 					const unitDir = dir.Unit;
-					dir = unitDir.mul(part.Size.Magnitude);
-					const [, nextPos] = Workspace.FindPartOnRayWithIgnoreList(
-						new Ray(pos, dir),
-						physicsIgnore,
-						false,
-						true,
-					);
-					const [, exit] = Workspace.FindPartOnRayWithIgnoreList(
-						new Ray(nextPos, dir.mul(-1)),
-						physicsIgnore,
-						false,
-						true,
-					);
-					const dist = unitDir.Dot(exit.sub(pos));
-					if (dist > 0) {
-						const currentVelocity = (ix * ix + iy * iy + iz * iz) ** 0.5;
-						if (dist < math.log(currentVelocity / this.minExitVelocity) / this.resistance) {
+					const exitDir = unitDir.mul(part.Size.Magnitude);
+					const [, nextPos] = Projectile.raycast(pos, exitDir, this.physicsIgnore);
+					const [, exit] = Projectile.raycast(nextPos, exitDir.mul(-1), this.physicsIgnore);
+					const distance = unitDir.Dot(exit.sub(pos));
+					if (distance > 0) {
+						const currentVelocity = new Vector3(vxOrig, vyOrig, vzOrig).Magnitude;
+						if (distance < math.log(currentVelocity / this.minExitVelocity) / this.resistance) {
 							this.px = exit.X;
 							this.py = exit.Y;
 							this.pz = exit.Z;
-							const gv = math.exp(-this.resistance * dist);
+
+							const gv = math.exp(-this.resistance * distance);
 							this.vx *= gv;
 							this.vy *= gv;
 							this.vz *= gv;
-						} else {
-							removeList.add(this);
-							this.px = pos.X;
-							this.py = pos.Y;
-							this.pz = pos.Z;
+							didPenetrate = true;
 						}
-					} else {
-						removeList.add(this);
-						this.px = nextPos.X;
-						this.py = nextPos.Y;
-						this.pz = nextPos.Z;
 					}
-				} else {
+				}
+
+				if (!didPenetrate) {
 					this.px = pos.X;
 					this.py = pos.Y;
 					this.pz = pos.Z;
+					if (this.bounce) {
+						const vDot = -2 * (this.vx * norm.X + this.vy * norm.Y + this.vz * norm.Z);
+						const RS = 0.9;
+						this.vx = RS * (vDot * norm.X + this.vx);
+						this.vy = RS * (vDot * norm.Y + this.vy);
+						this.vz = RS * (vDot * norm.Z + this.vz);
+					} else {
+						this.remove();
+					}
 				}
 
-				if (this.bounce) {
-					const nx = norm.X;
-					const ny = norm.Y;
-					const nz = norm.Z;
-					const vDot = -2 * (this.vx * nx + this.vy * ny + this.vz * nz);
-					const RS = 0.9;
-					this.vx = RS * (vDot * nx + this.vx);
-					this.vy = RS * (vDot * nx + this.vy);
-					this.vz = RS * (vDot * nx + this.vz);
-				} else {
-					removeList.add(this);
-				}
-
-				if (this.onTouch && this.onTouch(part, pos, norm)) {
-					this.line.Adornee = undefined;
-					removeList.add(this);
+				if (this.onTouch && this.onTouch(part, pos, norm) === true) {
+					this.remove(true);
 					return;
 				}
 			} else {
-				this.px += dx;
-				this.py += dy;
-				this.pz += dz;
+				this.px = pos.X;
+				this.py = pos.Y;
+				this.pz = pos.Z;
 			}
 
-			dx = this.px - qx;
-			dy = this.py - qy;
-			dz = this.pz - qz;
-
-			this.distanceSq += dx * dx + dy * dy + dz * dz;
-			if (this.distanceSq > this.maxRangeSq) {
-				removeList.add(this);
-				if (this.wasRendered) {
-					this.wasRendered = false;
-					this.line.Adornee = undefined;
-				}
-				return;
-			}
-
-			const oldPos = new Vector3(qx, qy, qz);
-			dir = new Vector3(dx, dy, dz);
-
-			const length = dir.Magnitude;
-			this.line.CFrame = new CFrame(oldPos, oldPos.add(dir)).mul(new CFrame(0, 0, -length / 2));
-
-			if (!this.wasRendered) {
-				this.wasRendered = true;
-				this.line.Adornee = terrain;
-			}
+			// actual delta
+			dx = this.px - pxOrig;
+			dy = this.py - pyOrig;
+			dz = this.pz - pzOrig;
+		} else {
+			// no collision, move without raycasting
+			this.px += dx;
+			this.py += dy;
+			this.pz += dz;
 		}
+
+		// remove if distance is too far
+		this.distanceSq += dx ** 2 + dy ** 2 + dz ** 2;
+		if (this.distanceSq > this.maxRangeSq) {
+			this.remove();
+			return;
+		}
+
+		// render
+		const origPos = new Vector3(pxOrig, pyOrig, pzOrig);
+		const direction = new Vector3(dx, dy, dz);
+		const length = direction.Magnitude;
+		this.line.Height = length;
+		this.line.CFrame = new CFrame(origPos, origPos.add(direction)).mul(new CFrame(0, 0, -length / 2));
 	}
 
-	public remove() {
-		removeList.add(this);
+	public remove(instantly = false) {
+		if (instantly) {
+			this.line.Adornee = undefined;
+		}
+		Projectile.removeList.add(this);
 	}
 }
 
-function update(dt: number) {
-	elapsedTime += dt;
-	let j = 0;
-	for (let i = 0; i < particles.size(); i++) {
-		const p = particles[i];
-		particles.remove(i);
-		if (removeList.has(p) || p.lifeTime < elapsedTime) {
+const raycastIgnore = Workspace.FindFirstChild("Ignore");
+if (raycastIgnore) {
+	Projectile.globalPhysicsIgnore.push(raycastIgnore);
+}
+
+RunService.RenderStepped.Connect(dt => {
+	Projectile.elapsedTime += dt;
+
+	const newProjectiles = new Array<Projectile>();
+	for (const p of Projectile.projectiles) {
+		if (Projectile.removeList.has(p) || p.lifeTime < Projectile.elapsedTime) {
 			p.line.Parent = undefined;
-			lines.push(p.line);
-			removeList.delete(p);
+			Projectile.lines.push(p.line);
+			Projectile.removeList.delete(p);
 		} else {
-			particles[j++] = p;
+			newProjectiles.push(p);
 			p.step(dt);
 		}
 	}
-}
 
-coroutine.wrap(() => {
-	while (true) {
-		wait(1);
-		update(1);
-	}
-})();
-
-// RunService.RenderStepped.Connect(dt => {
-// 	update(dt);
-// });
+	Projectile.projectiles = newProjectiles;
+});
